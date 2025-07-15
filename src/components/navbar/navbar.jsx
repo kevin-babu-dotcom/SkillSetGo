a3ad75b4f756e9ed24858e51178cbd07e2a80347
Kevin Babu <116744347+kevin-babu-dotcom@users.noreply.github.com>
2025-07-15 22:35:45 +0530

    Initial Commit

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+'use client'
+import {useState} from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
